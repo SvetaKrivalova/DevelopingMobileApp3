ede9dab1a5626be536749c90f181644a2ceca543
SvetaKrivalova <svetakrivalova123@gmail.com>
2022-11-24 14:33:37 +0700

    add

--- NestedLayoutsModificated/app/src/main/java/com/example/nestedlayouts/MainActivity.kt
+++ NestedLayoutsModificated/app/src/main/java/com/example/nestedlayouts/MainActivity.kt
@@ -4,44 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button_start: Button = findViewById(R.id.button_start)
-        val arrayText = arrayListOf<TextView>(findViewById(R.id.textView1))
-        arrayText.add(findViewById(R.id.textView2))
-        arrayText.add(findViewById(R.id.textView3))
-        arrayText.add(findViewById(R.id.textView4))
-        arrayText.add(findViewById(R.id.textView5))
-        arrayText.add(findViewById(R.id.textView6))
-        arrayText.add(findViewById(R.id.textView7))
-        arrayText.add(findViewById(R.id.textView8))
-        arrayText.add(findViewById(R.id.textView9))
-        var count = 0
-        var a = count+2
+
+        val button_start = (findViewById<Button>(R.id.button_start))
+        var list = arrayListOf<TextView>()
+        list.add(0, findViewById(R.id.textView1))
+        list.add(1, findViewById(R.id.textView2))
+        list.add(2, findViewById(R.id.textView3))
+        list.add(3, findViewById(R.id.textView4))
+        list.add(4, findViewById(R.id.textView5))
+        list.add(5, findViewById(R.id.textView6))
+        list.add(6, findViewById(R.id.textView7))
+        list.add(7, findViewById(R.id.textView8))
+        list.add(8, findViewById(R.id.textView9))
+        val following = 3
+        var clickCount = 0
+        var text = clickCount + 2
+
         button_start.setOnClickListener {
-            if(count == 2){
-                arrayText[count].setText("")
-                arrayText[count+3].setText("")
-                arrayText[count+6].setText("")
-                count = 0
-                arrayText[count].setText(a.toString())
-                arrayText[count+3].setText(a.toString())
-                arrayText[count+6].setText(a.toString())
-                a += 1
+            if (clickCount != 2) {
+                list[clickCount].setText(" ")
+                list[clickCount + following].setText(" ")
+                list[clickCount +following * 2 ].setText(" ")
+
+                list[clickCount + 1].setText(text.toString())
+                list[clickCount + following + 1].setText(text.toString())
+                list[clickCount +following * 2 + 1].setText(text.toString())
+
+                ++clickCount
+                ++text
             } else{
-                arrayText[count].setText("")
-                arrayText[count+3].setText("")
-                arrayText[count+6].setText("")
+                list[clickCount].setText(" ")
+                list[clickCount + following].setText(" ")
+                list[clickCount +following * 2 ].setText(" ")
 
-                arrayText[count+1].setText(a.toString())
-                arrayText[count+4].setText(a.toString())
-                arrayText[count+7].setText(a.toString())
+                clickCount = 0
+                text = 1
 
-                a += 1
-                count++
+                list[clickCount].setText(text.toString())
+                list[clickCount + following ].setText(text.toString())
+                list[clickCount +following * 2 ].setText(text.toString())
+
+                ++text
             }
         }
     }
